c68c1b0beb305fb025e613340599299bad43f40b
NasirUddin93 <nasir93cse@gmail.com>
2021-04-04 16:58:30 +0600

    he Admin login and Admin template setup

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('admin/layout')
+
+@section('container')
+    <div class="row">
+        <div class="col-lg-12" style="margin-left: 350px">
+          <h1>Dashboard</h1>
+        </div>
+    </div>
+@endsection
